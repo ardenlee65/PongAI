3cb09abf4c55136a5d3d9fafde27f0bbd49d455d
Arden Lee <ardenlee65@users.noreply.github.com>
2021-03-16 23:20:19 -0700

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -70,19 +70,7 @@ def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
     done = torch.ByteTensor(done)
-    # implement the loss function here
-    # loss = 0
-    # for x in range(batch_size):
-    #     target_val = reward.data[x] + gamma * torch.max(target_model(next_state).data[action.data[x]])
-    #     # Added this
-    #     if done.data[x]:
-    #         model_val = reward.data[x]
-    #     else:
-    #         # maybe dont use action.data[x]?
-    #         model_val = reward.data[x] + gamma * torch.max(model(state).data[action.data[x]])
-    #     loss += (target_val - model_val)**2
 
-    # return Variable(torch.FloatTensor([loss/batch_size]), requires_grad=True)
     state_action_values = model(state).gather(1, action.unsqueeze(-1)).squeeze(-1)
     next_state_values = target_model(next_state).max(1)[0].detach()
     next_state_values[done] = 0.0
